0e09a6d84322939d68e666cc9bdd6960092da13d
galoisz <galzfira@gmail.com>
2017-11-26 22:19:40 +0200

    add test message

--- WinApp1/Form1.cs
+++ WinApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WinApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello");
+            MessageBox.Show("hello for test");
         }
     }
 }
